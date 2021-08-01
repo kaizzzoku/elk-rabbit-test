59758c6de246a7beb73d603ee9f7144047999cac
kaizzzoku <z.zzzoku.z@gmail.com>
2021-08-01 19:11:19 +0300

    Create index page for articles

---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Articles</h1>
+    <form action="" class="w-100" method="GET">
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Articles" aria-describedby="button-addon2">
+            <div class="input-group-append">
+                <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Search</button>
+            </div>
+        </div>
+    </form>
+
+    @foreach($articles as $a)
+        <div class="card m-2">
+            <h4 class="m-2 card-title">{{ $a->title }}</h4>
+            <h5 class="m-2 card-subtitle text-muted">Author: {{ $a->author  }}</h5>
+        </div>
+    @endforeach
+    {{  $articles->links() }}
+@endsection
